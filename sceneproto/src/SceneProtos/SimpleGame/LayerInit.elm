981083c685a98eec0666ebe2c79f307fe80a4f47
linsy king <linsy_king@sjtu.edu.cn>
2023-05-18 14:59:39 +0800

    docs: fix layerinit

--- sceneproto/src/SceneProtos/SimpleGame/LayerInit.elm
+++ sceneproto/src/SceneProtos/SimpleGame/LayerInit.elm
@@ -1,14 +1,28 @@
-module SceneProtos.SimpleGame.LayerInit exposing (LayerInitData(..), SimpleGameInit)
+module SceneProtos.SimpleGame.LayerInit exposing
+    ( LayerInitData(..)
+    , SimpleGameInit
+    )
+
+{-|
+
+@docs LayerInitData
+@docs SumpleGameInit
+
+-}
 
 import Array exposing (Array)
 import SceneProtos.SimpleGame.GameComponent.Base exposing (GameComponent)
 
 
+{-| Layer init
+-}
 type LayerInitData
     = GameLayerInitData SimpleGameInit
     | NullLayerInitData
 
 
+{-| simple game init
+-}
 type alias SimpleGameInit =
     { balls : Array GameComponent
     , blackhole : Maybe GameComponent
